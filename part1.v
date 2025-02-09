b8d739723168c7353a8c24afbcc7d7bb580464b2
CollinRoss21 <cdr5451@psu.edu>
2025-02-08 20:00:33 -0500

    Changes width to size

--- part1.v
+++ part1.v
@@ -20,22 +20,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module part1 #(parameter width=32)(
-    input [width-1:0] a, b,
+module part1 #(parameter SIZE=16)(
+    input [SIZE-1:0] a, b,
     input cin,
-    output [width-1:0] s,
+    output [SIZE-1:0] s,
     output cout
     );
     
-    wire [width:0] carry;
+    wire [SIZE:0] carry;
     assign carry[0] = cin;
     
     genvar i;
     generate
-        for (i=0; i<width; i=i+1) begin : adder_resizer
+        for (i=0; i<SIZE; i=i+1) begin : adder_resizer
             part0 ra (.a(a[i]), .b(b[i]), .cin(carry[i]), .sum(s[i]), .cout(carry[i+1]));
         end
     endgenerate
-    assign cout = carry[width];
+    assign cout = carry[SIZE];
     
 endmodule
