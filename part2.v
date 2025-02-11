35862296e08cfb4f46db2733312dc9f74fe7c843
CollinRoss21 <cdr5451@psu.edu>
2025-02-11 02:04:36 -0500

    Got pipeline working but still delayed one cycle

--- part2.v
+++ part2.v
@@ -27,7 +27,7 @@ output [127:0] s,
 output cout
     );
     // 1-bit Carry register to hold carry values between 16-bit adder stages
-    reg [7:0]carry_reg;
+    reg [8:0]carry_reg;
     // Input register to hold input values a,b
     reg [127:0] a_reg [0:7];
     reg [127:0] b_reg [0:7];
@@ -37,6 +37,8 @@ output cout
     //Wires for the generate function
     wire [127:0] output_wire; //Output wire
     wire [7:0] carry_wire; //Carry wire
+    integer j;
+    integer k;
     
     genvar i;
     generate
@@ -44,28 +46,48 @@ output cout
             part1 #(.SIZE(16)) ac (
                 .a(a_reg[i][(i+1)*16-1:i*16]),
                 .b(b_reg[i][(i+1)*16-1:i*16]),
-                .cin(carry_reg[i]),
+                .cin(carry_reg[i]), //Set to cin for first adder
                 .s(output_wire[(i+1)*16-1:i*16]),
                 .cout(carry_wire[i])
             );
         end
     endgenerate
-    integer j;
+    
     always @(posedge clk) begin
-        // Assing the input values to the first reg
-        a_reg[0] = a;
-        b_reg[0] = b;
-        for (j = 0; j < 7; j = j + 1) begin
+    
+        // Assigning the input values to the first reg
+        a_reg[0] <= a;
+        b_reg[0] <= b;
+        carry_reg[0] <= cin;
+        
+        for (j = 0; j < 9; j = j + 1) begin
             carry_reg[j+1] <= carry_wire[j];
         end
         
-        
-        for (j = 0; j < 7; j = j + 1) begin //Shifts the input pipeline registers
+        for (j = 0; j < 8; j = j + 1) begin
             a_reg[j+1] <= a_reg[j];
             b_reg[j+1] <= b_reg[j];
-            output_reg[j+1] <= output_reg[j];
-        end
+        end         
         
+        for (j = 0; j < 8; j = j + 1) begin
+            output_reg[j+1] <= output_reg[j];
+        end 
+        
+        k = 0;
+        for (j = 0; j < 128; j = j + 1) begin
+            
+            output_reg[k][j] <= output_wire [j];
+            if ((j%16 == 15)&(j != 0)) begin
+                k = k + 1;
+            end
+  
+        end  
         
     end
+    
+        
+    // Makes the sum the last output
+    assign s = output_reg[7];
+    
+    assign cout = carry_reg[8];
 endmodule
